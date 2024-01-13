0b1b21e2bb6e5162e0a120db2ffea21e33eb65cf
DanSilSant <aluno23298@ipt.pt>
2024-01-13 21:08:24 +0000

    ver membros de um grupo

--- pythonAPI/app.py
+++ pythonAPI/app.py
@@ -39,6 +39,8 @@ def login():
 	conta=cursor.fetchone()
 
 	if conta == None:
+		cursor.close()
+		cnx.close()
 		return "Conta não existe",404
 	else:
 
@@ -48,10 +50,14 @@ def login():
 		
 		cursor.execute("update Users set token = '{}' where Username='{}'".format(newToken,username))
 		cnx.commit()
+		cursor.close()
+		cnx.close()
 		return jsonify({'username':username,
 			'token': newToken
 			}),200
 	else:
+		cursor.close()
+		cnx.close()
 		return "password errada",404
 
 
@@ -66,6 +72,8 @@ def register():
 	cursor.execute("select Username from Users where Username=%s",params)
 	conta=cursor.fetchone()
 	if conta != None:
+		cursor.close()
+		cnx.close()
 		return "Account already exists",400
 	else:
 		if args.get("secretAdmin") == "PasswordSecreta":
@@ -89,6 +97,8 @@ def register():
 		token= uuid4()
 		cursor.execute("insert into Users (Username,Password,Name,LastName,BirthDate,CreatedDate,Token,Admin) values ('{}','{}','{}','{}','{}','{}','{}','{}')".format(username,password,name,lastName,birthDate,createdDate,token,int(admin)))
 		cnx.commit()
+		cursor.close()
+		cnx.close()
 		return "",200
 
 @app.route('/users',methods=['GET'])
@@ -100,12 +110,15 @@ def getUsers(username=None):
 	if(username is None):
 		cursor.execute("select Username,Name,LastName,BirthDate,CreatedDate,Weight,Height,Address from Users")
 		conta=cursor.fetchall()
-		
+		cursor.close()
+		cnx.close()
 		return jsonify(conta)
 	else:
 		if(request.method=="GET"):
 			cursor.execute("select Username,Name,LastName,BirthDate,CreatedDate,Weight,Height,Address from Users where username = '{}'".format(username))
 			conta=cursor.fetchone()
+			cursor.close()
+			cnx.close()
 			return jsonify(conta)
 		if(request.method=="PUT"):
 			token = request.headers.get("auth")
@@ -120,10 +133,13 @@ def getUsers(username=None):
 						str+=","
 				cursor.execute("update Users set {} where username = '{}'".format(str,username))
 				cnx.commit()
-
+				cursor.close()
+				cnx.close()
 				return "",202
 			else:
 				print(request.args)
+				cursor.close()
+				cnx.close()
 				return "",404
 					
 
@@ -131,52 +147,103 @@ def getUsers(username=None):
 
 
 @app.route('/groups', methods=['GET','POST'])
-def getGroups():
+@app.route('/groups/<groupId>', methods=['GET','PUT','DELETE'])
+def getGroups(groupId=None):
 	cnx = mysql.connector.connect(user='root', password='Teste123!',host='16.170.180.240',port='3306',  database='app2',charset="utf8")
 	cursor=cnx.cursor(dictionary=True)
-	if request.method=='GET':		
-		cursor.execute("select * from RunGroups")
-		posts=cursor.fetchall()
-		return jsonify(posts)
+	if(groupId is None):
+		if request.method=='GET':		
+			cursor.execute("select * from RunGroups")
+			group=cursor.fetchall()
+			cursor.close()
+			cnx.close()
+			return jsonify(group)
 
+		if request.method=='POST':
+			args = request.args
+			name = args.get("name")
+			city=args.get("city")
+			createdDate=datetime.now()
+			print(createdDate)
+			cursor.execute("select Name from RunGroups where LOWER(REPLACE(Name,' ','')) = LOWER(REPLACE('{}',' ',''))".format(name))
+			groupName = cursor.fetchone()
+			
+			if groupName is not None:
+				cursor.close()
+				cnx.close()
+				return "groupName_exists",404		
+			token = request.headers.get("auth")
+			cursor.execute("select UserId from Users where Token = '{}'".format(token))
+			userId = cursor.fetchone()
+			if userId is None:
+				cursor.close()
+				cnx.close()
+				return "token_not_found", 404
+			else:
+				ownerId=userId['UserId']
+				print(city)
+				cursor.execute("insert into RunGroups (Name,City,OwnerId,CreatedDate) values ('{}','{}','{}','{}')".format(name,city,ownerId,createdDate))			
+				cnx.commit()
+				cursor.execute("select GroupId from RunGroups where Name like '{}'".format(name))
+				groupId = cursor.fetchone()['GroupId']
+				cursor.execute("insert into GroupMembers (GroupId,UserId,GroupAdmin) values ('{}','{}','{}')".format(groupId,ownerId,int(True)))
+				cnx.commit()
+				cursor.close()
+				cnx.close()
+				return "", 200
+		cursor.close()
+		cnx.close()		
+		return "",404
+	else:
+		if(request.method=="GET"):
+			cursor.execute("select Name, City, OwnerId, CreatedDate from RunGroups where Name ='{}'".format(groupId))
+			group=cursor.fetchone()
+			cursor.close()
+			cnx.close()
+			return jsonify(group)
+
+		if(request.method=="PUT"):
+			return
+
+		if(request.method=="DELETE"):
+			token = request.headers.get("auth")
+			cursor.execute("select OwnerId from RunGroups where Name = '{}'".format(groupId))
+			owner=cursor.fetchone()['OwnerId']
+			cursor.execute("select Token from Users where UserId = '{}'".format(owner))
+			token2=cursor.fetchone()['Token']			
+			if(token == token2):
+				cursor.execute("delete from RunGroups where Name ='{}'".format(groupId))
+				cnx.commit()
+				cursor.close()
+				cnx.close()
+				return "group_deleted",200	
+			else:
+				return "No_Permission",403
+		else:
+			cursor.close()
+			cnx.close()
+			return "",404
+
+@app.route('/groups/<groupId>/members', methods=['GET','POST'])
+def getGroupMembers(groupId):
+	cnx = mysql.connector.connect(user='root', password='Teste123!',host='16.170.180.240',port='3306',  database='app2')
+	cursor=cnx.cursor(dictionary=True)	
+	if request.method=='GET':	
+		cursor.execute("select u.Username from GroupMembers g inner join Users u on g.UserId = u.UserId where g.GroupId='{}'".format(groupId))
+		groupMembersId=cursor.fetchall()
+		print(groupMembersId)
+		cursor.close()
+		cnx.close()
+		return jsonify(groupMembersId)			
 	if request.method=='POST':
-		args = request.args
-		name = args.get("name")
-		city=args.get("city")
-		createdDate=datetime.now()
-		print(createdDate)
-		cursor.execute("select Name from RunGroups where LOWER(REPLACE(Name,' ','')) = LOWER(REPLACE('{}',' ',''))".format(name))
-		groupName = cursor.fetchone()
-		if groupName is not None:
-			return "groupName_exists",404		
 		token = request.headers.get("auth")
 		cursor.execute("select UserId from Users where Token = '{}'".format(token))
-		userId = cursor.fetchone()
-		if userId is None:
-			return "token_not_found", 404
-		else:
-			ownerId=userId['UserId']
-			print(city)
-			cursor.execute("insert into RunGroups (Name,City,OwnerId,CreatedDate) values ('{}','{}','{}','{}')".format(name,city,ownerId,createdDate))			
-			cnx.commit()
-			cursor.execute("select GroupId from RunGroups where Name like '{}'".format(name))
-			groupId = cursor.fetchone()['GroupId']
-			cursor.execute("insert into GroupMembers (GroupId,UserId,GroupAdmin) values ('{}','{}','{}')".format(groupId,ownerId,int(True)))
-			cnx.commit()
-
-			return "", 200
-	return "",404
-
-
-#@app.route('/group/<groupId>')
-#def addToGroup(groupId):
-#	cnx = mysql.connector.connect(user='root', password='Teste123!',host='16.170.180.240',port='3306',  database='app2')
-#	cursor=cnx.cursor(dictionary=True)
-#	args = request.args
-#	groupId=(groupId)
-#	cursor.execute("select Username,Name,LastName,BirthDate,CreatedDate from Users where username =%s",params)
-#	conta=cursor.fetchone()
-#	return jsonify(conta)
+		userId = cursor.fetchone()['UserId']
+		cursor.execute("insert into GroupMembers (GroupId,UserId,GroupAdmin) values ('{}','{}','{}')".format(groupId,userId,int(False)))
+		cnx.commit()
+		cursor.close()
+		cnx.close()
+		return "Added_to_Group", 200
 
 
 
@@ -189,6 +256,8 @@ def getPosts():
 	if request.method=='GET':		
 		cursor.execute("select * from Posts")
 		posts=cursor.fetchall()
+		cursor.close()
+		cnx.close()
 		return jsonify(posts)	
 
 	if request.method=='POST':
@@ -201,6 +270,8 @@ def getPosts():
 		cursor.execute("select * from GroupMembers where userid = {} and groupid = {}".format(userId, groupId))
 		query = cursor.fetchone()
 		if query.rowcount == 0:
+			cursor.close()
+			cnx.close()
 			return "",404
 		else:
 			cursor.execute("select Token from Users where UserId = {}".format(userId))
@@ -209,17 +280,29 @@ def getPosts():
 				cursor.execute("insert into Posts (Text,CreatedDate,UserId,GroupId,PhotoId) values ('{}','{}','{}','{}','{}')"
 				.format(text,createdDate,userId,groupId,photoId))
 				cnx.commit()
+				cursor.close()
+				cnx.close()
 				return "", 200
 			else:
+				cursor.close()
+				cnx.close()
 				return "", 502
 
-
-
-
+#@app.route('/user/posts')
 
 #@app.route('/group/<groupId>/posts', methods=['GET','POST'])
 
-#@app.route('/user/posts')
+
+#@app.route('/GroupMembers', methods=['GET'])
+#def getGroupMembers():
+#	cnx = mysql.connector.connect(user='root', password='Teste123!',host='16.170.180.240',port='3306',  database='app2')
+#	cursor=cnx.cursor(dictionary=True)			
+#	cursor.execute("select * from GroupMembers")
+#	groupMembers=cursor.fetchall()
+#	cursor.close()
+#	cnx.close()
+#	return jsonify(groupMembers)	
+
 
 
 
