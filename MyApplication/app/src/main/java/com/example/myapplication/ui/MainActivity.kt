8191f00cf7ce4541628942bd7d4864707ffa9634
DanSilSant <aluno23298@ipt.pt>
2024-01-18 21:13:46 +0000

    update mainactivity.kt

--- MyApplication/app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -20,11 +20,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: UserViewModel by viewModels()
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(LoginFragment())
         //replaceFragment(LoginFragment())
 
 
@@ -32,26 +32,26 @@ class MainActivity : AppCompatActivity() {
             hideKeyboard(it)
         }
 
-viewModel.loggedIn.observe(this, Observer {
-    if(it==true ){
-        login()
-    }else{
-        replaceFragment(LoginFragment(),"login")
+        viewModel.loggedIn.observe(this, Observer {
+            if(it==true ){
+                login()
+            }else{
+                replaceFragment(LoginFragment())
 
-    }
-})
+            }
+        })
 
         binding.bottomNavView.setOnItemSelectedListener {
 
             when(it.itemId){
-                R.id.item_groups -> replaceFragment(GroupsFragment(),"groups")
-                R.id.item_login -> replaceFragment(LoginFragment(),"login")
-                R.id.item_dashboard -> replaceFragment(DashBoardFragment(),"dashboard")
-                R.id.item_my_groups -> replaceFragment(GroupsFragment(),"groups")
+                R.id.item_groups -> replaceFragment(GroupsFragment())
+                R.id.item_login -> replaceFragment(LoginFragment())
+                R.id.item_dashboard -> replaceFragment(DashBoardFragment())
+                R.id.item_my_groups -> replaceFragment(GroupsFragment())
                 R.id.item_logout -> logout()
             }
 
-        true
+            true
         }
 
     }
@@ -62,19 +62,19 @@ viewModel.loggedIn.observe(this, Observer {
         binding.bottomNavView.menu.clear()
         binding.bottomNavView.inflateMenu(R.menu.bottom_nav)
         removeFragment(DashBoardFragment())
-        viewModel.loadGroups()
         Toast.makeText(this,"Logged Out Successful!",Toast.LENGTH_LONG).show()
 
 
     }
 
     private fun login(){
+        replaceFragment(GroupsFragment())
+        removeFragment(LoginFragment())
 
         supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
         binding.bottomNavView.menu.clear()
         binding.bottomNavView.inflateMenu(R.menu.loggedin_bottom_nav)
-        replaceFragment(DashBoardFragment(),"dashboard")
-        viewModel.user.value?.let { viewModel.loadUserGroups(it.userId) }
+        replaceFragment(DashBoardFragment())
 
     }
     companion object{
@@ -85,31 +85,11 @@ viewModel.loggedIn.observe(this, Observer {
     }
 
 
-    private fun replaceFragment(fragment: Fragment,tag:String = ""){
+    private fun replaceFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        val  added_frags = fragmentManager.fragments;
-        var added = false
-        for(frag  in added_frags) {
-            if(frag.tag == tag){
-                added=true
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
 
-                fragmentTransaction.show(frag)
-                fragmentManager.popBackStack()
-
-            } else{
-                fragmentTransaction.hide(frag)
-                fragmentTransaction.addToBackStack(null)
-
-
-            }
-
-        }
-
-        if (!added) {
-            fragmentTransaction.add(R.id.frame_layout, fragment,tag);
-        }
-        print(fragmentManager.backStackEntryCount)
         fragmentTransaction.addToBackStack(null).commit()
 
     }
